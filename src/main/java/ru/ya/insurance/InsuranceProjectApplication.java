d77e40a4c6ea31902f447a2ae9e750d4bd1874ad
Andrey-VU <avustjuzhanin@yandex.ru>
2023-11-25 11:20:46 +0300

    initial commit

---
+++ src/main/java/ru/ya/insurance/InsuranceProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.ya.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceProjectApplication.class, args);
+	}
+
+}
