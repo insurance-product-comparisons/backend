0602792d81975dbf3eb14f2aab650e59334abc6d
bannovdaniil <baddan@mail.ru>
2023-11-27 23:43:01 +0300

    github workflows

---
+++ src/main/java/ru/ya/insurance/controller/HelloController.java
@@ -0,0 +1,13 @@
+package ru.ya.insurance.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return "hello";
+    }
+}
