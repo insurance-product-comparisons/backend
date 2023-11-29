077a9745bb4e96d53d45bbd69650c6616edaf33a
bannovdaniil <baddan@mail.ru>
2023-11-30 02:41:52 +0300

    add  1. posgress docker dependency  2. properties -> yaml

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM amazoncorretto:17-alpine-jdk
 COPY target/insurance-*.jar insurance.jar
-ENTRYPOINT ["java","-jar","/insurance.jar"] 
-
+ENTRYPOINT ["java", "-jar", "/insurance.jar"] 
--- src/main/java/ru/ya/insurance/InsuranceProjectApplication.java
+++ src/main/java/ru/ya/insurance/InsuranceProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InsuranceProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InsuranceProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceProjectApplication.class, args);
+    }
 
 }
