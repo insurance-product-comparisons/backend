ead0fbafb5f6d9eb36280972224295193a808083
bannovdaniil <baddan@mail.ru>
2023-12-15 01:38:11 +0300

    Region Mapper

--- src/main/java/ru/ya/insurance/mapper/osago/RegionCoefficientMapper.java
+++ src/main/java/ru/ya/insurance/mapper/osago/RegionCoefficientMapper.java
@@ -3,14 +3,14 @@ package ru.ya.insurance.mapper.osago;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
-import ru.ya.insurance.dto.osago.SubRegionDto;
+import ru.ya.insurance.dto.osago.RegionCoefficientDto;
 import ru.ya.insurance.model.osago.RegionCoefficient;
 
 import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public interface SubRegionMapper {
-    SubRegionDto regionCoefficientToSubRegionDto(RegionCoefficient regionCoefficient);
+public interface RegionCoefficientMapper {
+    RegionCoefficientDto toDto(RegionCoefficient regionCoefficient);
 
-    List<SubRegionDto> regionCoefficientListToSubregionDtoList(List<RegionCoefficient> regionCoefficient);
+    List<RegionCoefficientDto> toDtoList(List<RegionCoefficient> regionCoefficientList);
 }
